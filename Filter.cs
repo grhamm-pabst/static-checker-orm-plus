3eec68a1e9c4db24943851193b7d9de01c09e791
unknown <grhammpabst456@gmail.com>
2023-06-09 20:42:37 -0300

    classe filter

---
+++ Filter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Checker
+{
+    internal class Filter
+    {
+        public List<char> validCharacters = new List<char>() { '_', ' ', '$', '%', '=', '>', '<', '!', '#', '?', ';', ':', ',', '{', '}', '[', ']', '(', ')', '*', '-', '+', '/' };
+
+        public bool isValid(char c)
+        {
+            return char.IsLetterOrDigit(c) || validCharacters.Contains(c);
+        }
+
+        public string filterFromString(string input)
+        {
+            IEnumerable<char> filteredString = input.Where(c => isValid(c));
+            return new string(filteredString.ToArray());
+        }
+    }
+}
